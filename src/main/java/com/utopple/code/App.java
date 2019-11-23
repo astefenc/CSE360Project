014ad9881417198341f5524f4862d5e1096d2e08
thomas <rnordman+github@protonmail.com>
2019-11-23 11:45:49 -0700

    Initlaize Maven Project.

---
+++ src/main/java/com/utopple/code/App.java
@@ -0,0 +1,13 @@
+package com.utopple.code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
