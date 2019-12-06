065ce2a7e90a7492732cbe7a93b085e0f16efb9f
Angela Foley <angelacollen8@gmail.com>
2019-12-05 17:27:41 -0700

    Mode test for lists

--- src/main/java/GradeAnalyzerTest.java
+++ src/main/java/GradeAnalyzerTest.java
@@ -1,6 +1,10 @@
 import org.junit.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -10,8 +14,10 @@ class GradeAnalyzerTest {
     // create dummy test data
     GradeAnalyzer gradeAnalyzer1 = new GradeAnalyzer();
     GradeAnalyzer gradeAnalyzer2 = new GradeAnalyzer();
+    GradeAnalyzer gradeAnalyzer3 = new GradeAnalyzer();
     List<Double> values1 = new LinkedList<>();
     List<Double> values2 = new LinkedList<>();
+    List<Double> values3 = new LinkedList<>();
 
     //TODO:Ange; Make test lists more robust, then calculate actual values, and use those in tests.
 
@@ -25,6 +31,7 @@ class GradeAnalyzerTest {
         values1.add(32.0113);
         values1.add(69.4330);
         values1.add(84.21);
+        values1.add(85.4);
         values1.add(85.4);
         values1.add(87.90);
         values1.add(84.21);
@@ -48,6 +55,17 @@ class GradeAnalyzerTest {
         gradeAnalyzer2.setLowerBound(10.0);
         gradeAnalyzer2.setUpperBound(50.0);
 
+        gradeAnalyzer3.addValue(3);
+        gradeAnalyzer3.addValue(7);
+        gradeAnalyzer3.addValue(7);
+        gradeAnalyzer3.addValue(9);
+        gradeAnalyzer3.addValue(7);
+        gradeAnalyzer3.addValue(5);
+        gradeAnalyzer3.addValue(0);
+
+        gradeAnalyzer3.setLowerBound(0);
+        gradeAnalyzer3.setUpperBound(10);
+        gradeAnalyzer3.setValues(values3);
     }
 
     @Test
@@ -90,19 +108,32 @@ class GradeAnalyzerTest {
         assertEquals(gradeAnalyzer2.getMedian(), median2, .001);
     }
 
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
     @Test
     void testGetMode() {
-        double mode1 = 0;
-        double mode2 = 0;
+        //multiple modes
+        List<Double> actualModes1 = gradeAnalyzer1.getMode();
+        assertThat(actualModes1, hasItems(85.4, 84.21));
+        assertEquals(actualModes1.size(), 2);
 
-        assertEquals(gradeAnalyzer1.getMode(), mode1, .001);
-        assertEquals(gradeAnalyzer2.getMode(), mode2, .001);
+        // No mode
+        gradeAnalyzer2.getMode();
+        exceptionRule.expectMessage("No mode.");
+
+        // One mode
+        assertThat(gradeAnalyzer3.getMode(), hasItems(7));
+        assertEquals(gradeAnalyzer3.getMode().size(), 1);
+
     }
 
     @Test
     void testAddValue() {
+        //TODO - where are values getting added (front or end of list)? Try one case where value added is legal
 
-
+        gradeAnalyzer2.addValue(63);
+        exceptionRule.expectMessage("Input value(s) out of range.");
     }
 
 
